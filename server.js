0a1e40453a6e824806104f69998059188fb3a255
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-06-11 22:46:05 +0200

    new changes

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -24,10 +25,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-// Index
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+// Index Routes
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 
 /* ***********************
